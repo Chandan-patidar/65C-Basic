a982b6434ad9f5701e6496f58ed59acd0bd2eeda
Chandan Patidar <88586932+Chandan-patidar@users.noreply.github.com>
2021-09-21 17:22:56 +0530

    Add files via upload

---
+++ basic/NumberEnterByUser.cpp
@@ -0,0 +1,9 @@
+//Write a program to Print Integer Number Enter by User
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    cout<<num;
+    return 0;
+}
---
+++ basic/helloworld.cpp
@@ -0,0 +1,7 @@
+//Write a program to print "Hello,World!"
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Hello,world!";
+    return 0;
+}
