7f791ef1fddaad033f7fb2000ca011fcb7e72e91
Chandan Patidar <88586932+Chandan-patidar@users.noreply.github.com>
2021-09-22 22:22:40 +0530

    Add files via upload

---
+++ basic/6-SwapTwonum.cpp
@@ -0,0 +1,13 @@
+//Write a program to swap Two Number.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a=8,b=4,temp;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"a ="<<a<<endl<<"b ="<<b<<endl;
+    return 0;
+
+}
