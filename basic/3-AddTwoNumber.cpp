33e9cd5e977fc80800dd7e62700a43ab34bfd27f
Chandan Patidar <88586932+Chandan-patidar@users.noreply.github.com>
2021-09-22 21:58:33 +0530

    Add files via upload

---
+++ basic/3-AddTwoNumber.cpp
@@ -0,0 +1,12 @@
+//Write a program to Add Two Number Entered by User.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2,add;
+    cin>>num1>>num2;
+    add=num1+num2;
+    cout<<add;
+    return 0;
+
+}
