2ae7e56c13a69a7107c1e7b5794a78eaeb87dbc6
Chandan Patidar <88586932+Chandan-patidar@users.noreply.github.com>
2021-09-23 19:29:22 +0530

    Add files via upload

---
+++ basic/7-FindASCIIvalue.cpp
@@ -0,0 +1,11 @@
+// Q6 Write a Program to Find ASCII value of a character.
+
+#include<iostream>
+using namespace std;
+int main(){
+    char c;
+    cout<<"Enter a character :";
+    cin>>c ;
+    cout<<"ASCII value of "<<c<<"is"<<int(c);
+    return 0;
+}
