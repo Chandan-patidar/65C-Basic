002de47b4abe0b1bdf8087e0297563d2deb56f7c
Chandan Patidar <88586932+Chandan-patidar@users.noreply.github.com>
2021-09-23 20:27:18 +0530

    Add files via upload

---
+++ if-else/1-numberisEvenOdd.cpp
@@ -0,0 +1,28 @@
+// Q1 Write a program to check Whether Number is Even or odd.
+
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter an integer :";
+	cin>>n;
+	
+	if (n%2==0)
+	    cout<< n << " "<<"is Even";
+	else
+	    cout<< n <<" "<<" is odd";
+	    
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
